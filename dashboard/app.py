fff5951f44265f85197c48fe98e69aa5f9d3c459
ElijahWandimi <wandimiem7@gmail.com>
2023-05-04 19:26:24 +0300

    add probablities to dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -6,13 +6,13 @@ import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output
 import time
-from .data_utils import scrape_data
+from .data_utils import scrape_data, read_static_data
 
 # Import data
 try:
     df = scrape_data()
 except ValueError:
-    df = pd.read_csv('..data/matches.csv')
+    df = read_static_data()
 
 df['Result'].fillna('D', inplace=True)
 
@@ -46,8 +46,8 @@ fig.add_trace(go.Indicator(
 ))
 fig.update_layout(
     grid={'rows': 1, 'columns': 3, 'pattern': "independent"},
+    title = 'Propotion of Results',
     template={'data': {'indicator': [{
-        'title': {'text': "Results"},
         'mode': "number+delta+gauge",
         'delta': {'reference': df['Result'].value_counts().sum()},
         'gauge': {'axis': {'visible': False}},
@@ -75,7 +75,7 @@ opp_fig.add_trace(go.Bar(
 ))
 opp_fig.update_layout(
     barmode='stack',
-    title='Results against opponents'
+    title='Results Distribution by Opponent'
 )
 
 # results for each day
@@ -98,7 +98,7 @@ day_fig.add_trace(go.Bar(
 ))
 day_fig.update_layout(
     barmode='stack',
-    title='Results by day'
+    title='Results Distribution by  day'
 )
 
 # probability of each outcome
@@ -127,12 +127,72 @@ day_prob_fig.add_trace(go.Indicator(
 ))
 day_prob_fig.update_layout(
     grid={'rows': 1, 'columns': 3, 'pattern': "independent"},
+    title = 'Probability of each outcome',
     template={'data': {'indicator': [{
-        'title': {'text': "Results"},
         'mode': "number",
         'domain': {'row': 0, 'column': 0}}]
     }}
 )
+
+# venue results 
+venue_df = df.groupby('Venue')['Result'].value_counts().unstack().fillna(0)
+venue_fig = go.Figure()
+venue_fig.add_trace(go.Indicator(
+    mode='number',
+    value=venue_df['W'][0],
+    title='Home wins',
+    domain={'row': 0, 'column': 0}
+))
+venue_fig.add_trace(go.Indicator(
+    mode='number',
+    value=venue_df['D'][0],
+    title='Home draws',
+    domain={'row': 0, 'column': 1}
+))
+venue_fig.add_trace(go.Indicator(
+    mode='number',
+    value=venue_df['L'][0],
+    title='Home losses',
+    domain={'row': 0, 'column': 2}
+))
+venue_fig.update_layout(
+    grid={'rows': 1, 'columns': 3, 'pattern': "independent"},
+    title = 'Outcomes for home games',
+    template={'data': {'indicator': [{
+        'mode': "number",
+        'domain': {'row': 0, 'column': 0}}]
+    }}
+)
+
+# away
+away_fig = go.Figure()
+away_fig.add_trace(go.Indicator(
+    mode='number',
+    value=venue_df['W'][1],
+    title='Away wins',
+    domain={'row': 0, 'column': 0}
+))
+away_fig.add_trace(go.Indicator(
+    mode='number',
+    value=venue_df['D'][1],
+    title='Away draws',
+    domain={'row': 0, 'column': 1}
+))
+away_fig.add_trace(go.Indicator(
+    mode='number',
+    value=venue_df['L'][1],
+    title='Away losses',
+    domain={'row': 0, 'column': 2}
+))
+away_fig.update_layout(
+    grid={'rows': 1, 'columns': 3, 'pattern': "independent"},
+    title = 'Outcomes for away games',
+    template={'data': {'indicator': [{
+        'mode': "number",
+        'domain': {'row': 0, 'column': 0}}]
+    }}
+)
+
 
 
 
@@ -141,26 +201,34 @@ app.layout = html.Div([
     html.H1("MANCHESTER UNITED PREMIER LEAGUE STATS"),
 
     html.Div([
-        html.H3("Results"),
         dcc.Graph(id='results', figure=fig)
     ], style={'width': '49%', 'display': 'inline-block', 'height': '50%'}),
 
     html.Div([
-        html.H3("Results by opponent"),
         dcc.Graph(id='goals', figure=opp_fig)
     ], style={'width': '49%', 'display': 'inline-block', 'height': '50%'}),
 
     html.Br(),
 
     html.Div([
-        html.H3("Results by day"),
         dcc.Graph(id='day', figure=day_fig)
     ], style={'width': '49%', 'display': 'inline-block', 'height': '50%'}),
 
     html.Div([
-        html.H3("Outcome probability"),
         dcc.Graph(id='day-prob', figure=day_prob_fig)
-    ], style={'width': '49%', 'display': 'inline-block', 'height': '50%'})
+    ], style={'width': '49%', 'display': 'inline-block', 'height': '50%'}),
+
+    html.Br(),
+
+    html.Div([
+        html.Div([
+            dcc.Graph(id='venue', figure=venue_fig)
+        ], style={'width': '49%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Graph(id='away', figure=away_fig)
+        ], style={'width': '49%', 'display': 'inline-block', 'height': '50%'}),
+    ], style={'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}),
 
 ])
 
